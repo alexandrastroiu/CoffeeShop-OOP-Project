54ae912587f9dd54379c5dcd3df509f0a6967b5d
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2024-12-25 19:24:14 +0200

    Added  order.hpp and employee.hpp files
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

---
+++ headers/employee.hpp
@@ -0,0 +1,77 @@
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
+
+#include <string>
+
+class Employee {
+protected:
+    std::string name, role;
+    int shiftStart, shiftEnd;
+    float payment;
+
+public:
+    Employee(std::string name, std::string role, int start, int end, float payment);
+
+    virtual void showEmployeeInfo() = 0;     /// Abstraction
+    virtual void updateEmployeeHours(int start, int end) = 0;
+
+    virtual ~Employee(); /// Destructor
+};
+
+/// Inheritance
+class Manager : public Employee {
+public:
+    Manager(std::string name, std::string role, int start, int end, float payment);
+
+    void showEmployeeInfo() override;
+    void updateEmployeeHours(int start, int end) override;
+};
+
+class Barista : public Employee {
+public:
+    Barista(std::string name, std::string role, int start, int end, float payment);
+
+    void showEmployeeInfo() override;
+    void updateEmployeeHours(int start, int end)  override;
+};
+
+class Waiter : public Employee {
+public:
+    Waiter(std::string name, std::string role, int start, int end, float payment);
+
+    void showEmployeeInfo() override;  /// Polymorphism
+    void updateEmployeeHours(int start, int end) override;
+};
+
+/// Abstract Factory Pattern
+class EmployeeFactory {
+    public:
+    virtual Employee* createManager(std::string name, std::string role, int start, int end, float payment) = 0;
+    virtual Employee* createBarista(std::string name, std::string role, int start, int end, float payment) = 0;
+    virtual Employee* createWaiter(std::string name, std::string role, int start, int end, float payment) = 0;
+
+    virtual ~EmployeeFactory() = default; /// Destructor
+};
+
+class ManagerFactory: public EmployeeFactory {
+public:
+    Employee *createManager(std::string name, std::string role, int start, int end, float payment) override;
+    Employee *createBarista(std::string name, std::string role, int start, int end, float payment) override;
+    Employee *createWaiter(std::string name, std::string role, int start, int end, float payment) override;
+};
+
+class BaristaFactory: public EmployeeFactory {
+public:
+    Employee *createManager(std::string name, std::string role, int start, int end, float payment) override;
+    Employee *createBarista(std::string name, std::string role, int start, int end, float payment) override;
+    Employee *createWaiter(std::string name, std::string role, int start, int end, float payment) override;
+};
+
+class WaiterFactory: public EmployeeFactory {
+public:
+    Employee *createManager(std::string name, std::string role, int start, int end, float payment) override;
+    Employee *createBarista(std::string name, std::string role, int start, int end, float payment) override;
+    Employee *createWaiter(std::string name, std::string role, int start, int end, float payment) override;
+};
+
+#endif
---
+++ headers/order.hpp
@@ -0,0 +1,28 @@
+#ifndef ORDER_H
+#define ORDER_H
+
+#include "../headers/product.hpp"
+#include <string>
+#include <vector>
+
+class Order {
+    private:
+    std::string clientName;
+    std::vector<Product> products;
+    float totalPrice;
+
+    public:
+    Order(std::string clientName, std::vector<Product> products);  // Constructor
+
+    void showOrderInfo(); //Method
+    void setClientName(std::string name); // Setter
+    void setProducts(std::vector<Product> products); //Setter 
+    void setTotalPrice(float price);
+    std::string getClientName(); //Getter
+    std::vector<Product> getProducts(); // Getter
+    float calculateTotalPrice(); // Method
+
+    ~Order() = default;  // Default Destructor
+};
+
+#endif
