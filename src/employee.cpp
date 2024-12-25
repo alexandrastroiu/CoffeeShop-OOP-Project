359f8fba164e0cac9a8b733f4415f149b073208d
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2024-12-25 15:15:27 +0200

    Added implementation of the Employee class and subclasses methods in the employee.cpp file
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

---
+++ src/employee.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include "../headers/employee.hpp"
+
+using namespace std;
+
+// Employee constructor
+Employee::Employee(string name, string role, int start, int end, float payment) {
+      this->name = name;
+      this->role = role;
+      this->shiftStart = start;
+      this->shiftEnd = end;
+      this->payment = payment;
+}
+
+// Employee Destructor
+Employee::~Employee() {}
+
+// Manager Constructor
+Manager::Manager(string name, string role, int start, int end, float payment): Employee(name, role, start, end, payment) {}
+
+// Barista Constructor
+Barista::Barista(string name, string role, int start, int end, float payment): Employee(name, role, start, end, payment) {}
+
+// Waiter Constructor
+Waiter::Waiter(string name, string role, int start, int end, float payment): Employee(name, role, start, end, payment) {}
+
+// Methods
+void Manager::showEmployeeInfo() {
+    cout << "Manager Info: " << endl;
+    cout << "Name: " << this->name << " " << "Hours: " << this->shiftStart << " - " << this->shiftEnd << " Payment: " << this->payment << endl;
+}
+
+void Barista::showEmployeeInfo() {
+    cout << "Barista Info: " << endl;
+    cout << "Name: " << this->name << " " << "Hours: " << this->shiftStart << " - " << this->shiftEnd << " Payment: " << this->payment << endl;
+}
+
+void Waiter::showEmployeeInfo() {
+    cout << "Waiter Info: " << endl;
+    cout << "Name: " << this->name << " " << "Hours: " << this->shiftStart << " - " << this->shiftEnd << " Payment: " << this->payment << endl;
+}
+
+void Manager::updateEmployeeHours(int start, int end) {
+    cout << "Updated Manager hours." << endl;
+    this->shiftStart = start;
+    this->shiftEnd = end;
+}
+
+void Barista::updateEmployeeHours(int start, int end) {
+    cout << "Updated Barista hours." << endl;
+    this->shiftStart = start;
+    this->shiftEnd = end;
+}
+
+void Waiter::updateEmployeeHours(int start, int end) {
+    cout << "Updated Waiter hours." << endl;
+    this->shiftStart = start;
+    this->shiftEnd = end;
+} 
+
+Employee* ManagerFactory::createManager(std::string name, std::string role, int start, int end, float payment) {
+    return new Manager(name, role, start, end, payment);
+}
+
+Employee* ManagerFactory::createBarista(std::string name, std::string role, int start, int end, float payment) {
+    return nullptr;
+}
+
+Employee* ManagerFactory::createWaiter(std::string name, std::string role, int start, int end, float payment) {
+   return nullptr;
+}
+
+Employee* BaristaFactory::createBarista(std::string name, std::string role, int start, int end, float payment) {
+    return new Barista(name, role, start, end, payment);
+}
+
+Employee* BaristaFactory::createManager(std::string name, std::string role, int start, int end, float payment) {
+    return nullptr;
+}
+
+Employee* BaristaFactory::createWaiter(std::string name, std::string role, int start, int end, float payment) {
+    return nullptr;
+}
+
+Employee* WaiterFactory::createWaiter(std::string name, std::string role, int start, int end, float payment) {
+    return new Waiter(name, role, start, end, payment);
+}
+
+Employee* WaiterFactory::createManager(std::string name, std::string role, int start, int end, float payment) {
+    return nullptr;
+}
+
+Employee* WaiterFactory::createBarista(std::string name, std::string role, int start, int end, float payment) {
+   return nullptr;
+}
+
