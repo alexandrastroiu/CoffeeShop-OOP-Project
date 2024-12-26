2806362ff5bd1ff07c7afbf4fa3c1a6c22efee9d
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2024-12-26 12:25:58 +0200

    Add declaration of Event class and subclasses in the event.hpp header file
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

---
+++ headers/event.hpp
@@ -0,0 +1,67 @@
+#ifndef EVENT_H
+#define EVENT_H
+
+#include "../headers/product.hpp"
+#include <string>
+#include <vector>
+
+class Event {
+    protected:
+    std::vector<Product> products;
+    float totalEventCost;
+
+    public:
+    Event(std::vector<Product> products, float cost);  // Constructor
+
+    virtual float calculateEventCost() = 0;    // Abstraction
+    virtual void showEventInfo() = 0;
+
+    virtual ~Event() {}   // Destructor
+
+};
+
+// Inheritance
+class CoffeeTastingEvent: public Event {
+    private:
+    float marketingCost;
+
+    public:
+    CoffeeTastingEvent(std::vector<Product> products, float cost, float marketingCost);
+
+    float calculateEventCost() override;
+    void showEventInfo() override;
+};
+
+class LiveMusicEvent: public Event {
+    private:
+    float bandCost, marketingCost;
+
+    public:
+    LiveMusicEvent(std::vector<Product> products, float cost, float bandCost, float marketingCost);
+
+    float calculateEventCost() override;
+    void showEventInfo() override;
+};
+
+// Abstract Factory design pattern
+class EventFactory {
+    public:
+    virtual Event* createCoffeeTastingEvent(std::vector<Product> products, float cost, float marketingCost) = 0;
+    virtual Event* createLiveMusicEvent(std::vector<Product> products, float cost, float bandCost, float marketingCost) = 0;
+
+    virtual ~EventFactory() = default; // Destructor
+};
+
+class CoffeeTastingEventFactory: public EventFactory {
+    public:
+    Event* createCoffeeTastingEvent(std::vector<Product> products, float cost, float marketingCost) override;
+    Event* createLiveMusicEvent(std::vector<Product> products, float cost, float bandCost, float marketingCost) override;
+};
+
+class LiveMusicEventFactory: public EventFactory {
+    public:
+    Event* createCoffeeTastingEvent(std::vector<Product> products, float cost, float marketingCost) override;
+    Event* createLiveMusicEvent(std::vector<Product> products, float cost, float bandCost, float marketingCost) override;
+};
+
+#endif
