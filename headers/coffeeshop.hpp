dbc491eb639907da4ff7a1194e98644a286332f3
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2024-12-26 13:08:48 +0200

    Create Coffeshop class and declare attributes and methods in the coffeeshop.hpp file
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

---
+++ headers/coffeeshop.hpp
@@ -0,0 +1,47 @@
+#ifndef COFFEESHOP_H
+#define COFFEESHOP_H
+
+#include "../headers/product.hpp"
+#include "../headers/order.hpp"
+#include "../headers/employee.hpp"
+#include "../headers/event.hpp"
+#include <string>
+#include <vector>
+
+class Coffeeshop {
+    private:       // Encapsulation
+    std::string city;
+    int employeesNumber;
+    std::vector<Employee> allEmployees;
+    std::vector<Product> allProducts;
+    std::vector<Order> allOrders;
+    std::vector<Event> allEvents;
+    float dailyRevenue, dailyCost, dailyIncome, productsCost, employeesCost;
+    
+
+    public:
+    Coffeeshop();  // Constructor
+    
+    //TODO: ADD REPORT AND FILES , CHECK METHODS  AND ATTRIBUTES AGAIN
+    void setCity(std::string name);  // Setter
+    void setEmployeesNumber(int number);
+    std::string getCity();  // Getter
+    int getEmployeesNumber();
+    void showCoffeeshopInfo();
+    void addEmployee();
+    void deleteEmployee();
+    float calculateEmployeesCost();
+    void updateEmployeeHours();
+    void addProduct();
+    void deleteProduct();
+    float calculateProductsCost();
+    void organizeEvent();
+    float calculateDailyRevenue();
+    float calculateDailyCost();
+    float calculateDailyIncome();
+
+
+    ~Coffeeshop() = default;  // Default Destructor
+};
+
+#endif
