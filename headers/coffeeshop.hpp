597e9f61fef24726262556b8566935d7c2a9a96b
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2024-12-27 15:04:24 +0200

    Add more methods and modify method parameters for Coffeeshop class
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

--- headers/coffeeshop.hpp
+++ headers/coffeeshop.hpp
@@ -13,10 +13,10 @@ class Coffeeshop {
     private:       // Encapsulation
     std::string city;
     int employeesNumber;
-    std::vector<Employee> allEmployees;
+    std::vector<Employee*> allEmployees; ///TODO CHECK FOR *
     std::vector<Product> allProducts;
     std::vector<Order> allOrders;
-    std::vector<Event> allEvents;
+    std::vector<Event*> allEvents;
     float dailyRevenue, dailyCost, dailyIncome, productsCost, employeesCost;
     
 
@@ -29,21 +29,25 @@ class Coffeeshop {
     std::string getCity();  // Getter
     int getEmployeesNumber();
     void showCoffeeshopInfo();
-    void addEmployee();
-    void deleteEmployee();
-    void updateEmployeeHours();
-    float calculateEmployeesCost();
-    void addProduct();
-    void deleteProduct();
+    void getAllEmployees(std::string fileName, std::vector<Employee*>& allEmployees);
+    void addEmployee(std::string fileName, std::string name, std::string role, int start, int end, float payment);
+    void deleteEmployee(std::string fileName, std::string name, std::string role, int start, int end, float payment);
+    void updateEmployeeHours(std::string fileName, std::string name, std::string role, int start, int end, float payment, int newStart, int newEnd);
+    float calculateEmployeesCost(); ///TODO
+    void getAllProducts(std::string fileName, std::vector<Product>& allProducts);
+    void addProduct(std::string fileName, std::string productName, std::string productType, int quantity, float price, float cost);
+    void deleteProduct(std::string fileName,std::string productName, std::string productType, int quantity, float price, float cost);
     float calculateProductsCost();
-    void placeOrder();
-    void organizeEvent();
-    float calculateDailyRevenue();
-    float calculateDailyCost();
-    float calculateDailyIncome();
+    void getAllOrders(std::string fileName, std::vector<Order>& allOrders);
+    void placeOrder(std::string fileName, Order order); ///todo
+    void getAllEvents(std::string fileName, std::vector<Event*>& allEvents);
+    void organizeEvent(std::string fileName, Event* event); ///TODO
+    float calculateDailyRevenue(); //TODO
+    float calculateDailyCost();   //TODO  
+    float calculateDailyIncome();  //TODO
+    void createReport(std::string fileName, float revenue, float cost, float income, float productsCost, float employeesCost);
 
-
-    ~Coffeeshop() = default;  // Default Destructor
+   ~Coffeeshop(); // Destructor
 };
 
 #endif
