bef09826212baaa7772778f6b545439f7e2448cf
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2025-01-03 17:06:08 +0200

    Modify paths
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

--- src/main.cpp
+++ src/main.cpp
@@ -14,13 +14,13 @@ using namespace std;
 int main() {
     Coffeeshop coffeeshop("Bucharest",3); ///first test
 
-    coffeeshop.getAllEmployees("../../data/employees_bucharest.csv");
-    coffeeshop.addEmployee("../../data/employees_bucharest.csv","Radu Ion", "Waiter", 9, 17, 120);
-    coffeeshop.addEmployee("../../data/employees_bucharest.csv","Ana Dan", "Waiter", 9, 17, 120);
+    coffeeshop.getAllEmployees("../data/employees_bucharest.csv");
+    coffeeshop.addEmployee("../data/employees_bucharest.csv","Radu Ion", "Waiter", 9, 17, 120);
+    coffeeshop.addEmployee("../data/employees_bucharest.csv","Ana Dan", "Waiter", 9, 17, 120);
     // coffeeshop.deleteEmployee("../data/employees_bucharest.csv","Radu Ion", "Waiter", 9, 17, 120);
-    coffeeshop.updateEmployeeHours("../../data/employees_bucharest.csv","Ana Dan", "Waiter", 9, 17, 120, 9, 15);
-    coffeeshop.getAllProducts("../../data/products_bucharest.csv");
-    coffeeshop.addProduct("../../data/products_bucharest.csv", "Orange Juice", "Drinks", 5, 10, 5);
+    coffeeshop.updateEmployeeHours("../data/employees_bucharest.csv","Ana Dan", "Waiter", 9, 17, 120, 9, 15);
+    coffeeshop.getAllProducts("../data/products_bucharest.csv");
+    coffeeshop.addProduct("../data/products_bucharest.csv", "Orange Juice", "Drinks", 5, 10, 5);
 
     return 0;
 }
