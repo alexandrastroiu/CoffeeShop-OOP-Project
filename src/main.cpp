4dd5c928140ff1bf86c50ebb50f4a81d08d48058
Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>
2025-01-03 21:04:56 +0200

    Modify main.cpp
    Signed-off-by: Alexandra Stroiu <alexandra.stroiu@stud.acs.upb.ro>

--- src/main.cpp
+++ src/main.cpp
@@ -46,7 +46,7 @@ int main() {
         }
     }
     Order order("Ana Mihai", orderProducts);
-    cout << order.calculateTotalPrice("../../data/orders_bucharest.csv") << endl;
+    // cout << order.calculateTotalPrice("../../data/orders_bucharest.csv") << endl;
     coffeeshop.getAllOrders("../../data/orders_bucharest.csv");
     coffeeshop.showAllOrders();
     coffeeshop.placeOrder("../../data/orders_bucharest.csv", order);
